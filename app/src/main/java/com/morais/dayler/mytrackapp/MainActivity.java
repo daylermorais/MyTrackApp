8d8e347c94330fbfdfa3df9d2473c63794b85c60
dayler.morais <dayler.morais@gmail.com>
2015-04-23 12:13:15 -0300

    Add menu settings to change map type

--- app/src/main/java/com/morais/dayler/mytrackapp/MainActivity.java
+++ app/src/main/java/com/morais/dayler/mytrackapp/MainActivity.java
@@ -1,13 +1,110 @@
 package com.morais.dayler.mytrackapp;
 
-import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends ActionBarActivity {
+    private GoogleMap googleMap;
+    private int mapType = GoogleMap.MAP_TYPE_NORMAL;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        MapFragment mapFragment =  (MapFragment) fragmentManager.findFragmentById(R.id.map);
+        googleMap = mapFragment.getMap();
+
+        LatLng sfLatLng = new LatLng(37.7750, -122.4183);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sfLatLng)
+                .title("San Francisco")
+                .snippet("Population: 776733")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        LatLng sLatLng = new LatLng(37.857236, -122.486916);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sLatLng)
+                .title("Sausalito")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+
+
+        googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        LatLng cameraLatLng = sfLatLng;
+        float cameraZoom = 10;
+
+        if(savedInstanceState != null){
+            mapType = savedInstanceState.getInt("map_type", GoogleMap.MAP_TYPE_NORMAL);
+
+            double savedLat = savedInstanceState.getDouble("lat");
+            double savedLng = savedInstanceState.getDouble("lng");
+            cameraLatLng = new LatLng(savedLat, savedLng);
+
+            cameraZoom = savedInstanceState.getFloat("zoom", 10);
+        }
+
+        googleMap.setMapType(mapType);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(cameraLatLng, cameraZoom));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()){
+            case R.id.normal_map:
+                mapType = GoogleMap.MAP_TYPE_NORMAL;
+                break;
+
+            case R.id.satellite_map:
+                mapType = GoogleMap.MAP_TYPE_SATELLITE;
+                break;
+
+            case R.id.terrain_map:
+                mapType = GoogleMap.MAP_TYPE_TERRAIN;
+                break;
+
+            case R.id.hybrid_map:
+                mapType = GoogleMap.MAP_TYPE_HYBRID;
+                break;
+        }
+
+        googleMap.setMapType(mapType);
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save the map type so when we change orientation, the mape type can be restored
+        LatLng cameraLatLng = googleMap.getCameraPosition().target;
+        float cameraZoom = googleMap.getCameraPosition().zoom;
+        outState.putInt("map_type", mapType);
+        outState.putDouble("lat", cameraLatLng.latitude);
+        outState.putDouble("lng", cameraLatLng.longitude);
+        outState.putFloat("zoom", cameraZoom);
     }
 }
